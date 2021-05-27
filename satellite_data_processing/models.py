df31a5e37c25ff8433b261cc64fe176a50189a1c
TimKie <55922216+TimKie@users.noreply.github.com>
2021-05-27 10:16:35 +0200

    added download of all corresponding scenes after user selects one scene + added combination of bands and visualisation of final image

--- satellite_data_processing/models.py
+++ satellite_data_processing/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Location(models.Model):
-    location = models.CharField(max_length=100, blank=True, primary_key=True)
+    location = models.CharField(max_length=100, blank=True)
 
 
 class Indicator(models.Model):
-    indicator = models.CharField(max_length=100, blank=True, primary_key=True)
+    indicator = models.CharField(max_length=100, blank=True)
 
